56fc4e3afa8715dc0bc0c689791264ce665d106d
Administrator <2298598204@qq.com>
2019-08-21 20:15:13 +0800

    dengshilin 4

---
+++ src/main/java/com/meiyou/utils/RedisUtil.java
@@ -0,0 +1,16 @@
+package com.meiyou.utils;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @program: meiyou
+ * @description:
+ * @author: dengshilin
+ * @create: 2019-08-21 17:48
+ **/
+public class RedisUtil {
+
+
+}

698a9a3565a04ca806ebde33f34180da8080fc52
Xidian2850 <598109397@qq.com>
2019-08-21 11:58:17 +0800

    @EnableCaching//启用Redis缓存

--- src/main/java/com/meiyou/MeiyouApplication.java
+++ src/main/java/com/meiyou/MeiyouApplication.java
@@ -2,8 +2,10 @@ package com.meiyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching //启用Redis缓存
 public class MeiyouApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/meiyou/MeiyouApplicationTests.java
+++ src/test/java/com/meiyou/MeiyouApplicationTests.java
@@ -11,6 +11,7 @@ public class MeiyouApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }

0683f783d2d201cdc3c162200a5a7bf79a638235
Xidian2850 <598109397@qq.com>
2019-08-21 14:46:16 +0800

    1，添加动态Service接口 2，新增视图模型包Model

---
+++ src/main/java/com/meiyou/controller/ActivityController.java
@@ -0,0 +1,17 @@
+package com.meiyou.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ：huangzhaoyang
+ * @date ：Created in 2019/8/21 13:47
+ * @description：附近动态
+ * @modified By：huangzhaoyang
+ * @version: 1.0.0
+ */
+@Controller
+public class ActivityController {
+
+
+
+}
---
+++ src/main/java/com/meiyou/model/ActivityModel.java
@@ -0,0 +1,27 @@
+package com.meiyou.model;
+
+/**
+ * @author ：huangzhaoyang
+ * @date ：Created in 2019/8/21 14:38
+ * @description：动态视图模型
+ * @modified By：huangzhaoyang
+ * @version: 1.0.0
+ */
+public class ActivityModel {
+
+    //用户id
+    private int uid;
+
+    //用户头像
+    private String headPic;
+
+    //动态文字
+    private String content;
+
+    //动态图片
+    private String imgsUrl;
+
+    //距离
+    private String distance;
+
+}

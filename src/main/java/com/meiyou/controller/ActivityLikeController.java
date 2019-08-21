167613d1d621a0e573f95c53bcfc0f0c77b341ed
Xidian2850 <598109397@qq.com>
2019-08-21 21:25:53 +0800

    新增点赞接口

---
+++ src/main/java/com/meiyou/controller/ActivityLikeController.java
@@ -0,0 +1,15 @@
+package com.meiyou.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ：huangzhaoyang
+ * @date ：Created in 2019/8/21 21:15
+ * @description：动态点赞表
+ * @modified By：huangzhaoyang
+ * @version: 1.0.0
+ */
+@Controller
+public class ActivityLikeController {
+}
+
---
+++ src/main/java/com/meiyou/service/impl/ActivityLikeServiceImpl.java
@@ -0,0 +1,15 @@
+package com.meiyou.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ：huangzhaoyang
+ * @date ：Created in 2019/8/21 21:17
+ * @description：动态点赞服务层实现类
+ * @modified By：huangzhaoyang
+ * @version: 1.0.0
+ */
+@Service
+public class ActivityLikeServiceImpl {
+
+}

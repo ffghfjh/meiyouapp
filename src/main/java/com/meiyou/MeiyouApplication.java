3f8519946218f3f03326a375206f5fb90453f959
Administrator <2298598204@qq.com>
2019-08-21 11:10:02 +0800

    初始化

---
+++ src/main/java/com/meiyou/MeiyouApplication.java
@@ -0,0 +1,13 @@
+package com.meiyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeiyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeiyouApplication.class, args);
+    }
+
+}

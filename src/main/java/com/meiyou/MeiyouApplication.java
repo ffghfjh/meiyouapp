016d1adb154eeab6da31bd0dc114df7b67e6e66d
Administrator <2298598204@qq.com>
2019-08-21 13:27:46 +0800

    初始化

--- src/main/java/com/meiyou/MeiyouApplication.java
+++ src/main/java/com/meiyou/MeiyouApplication.java
@@ -1,9 +1,11 @@
 package com.meiyou;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.meiyou.mapper")
 public class MeiyouApplication {
 
     public static void main(String[] args) {

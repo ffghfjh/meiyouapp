1fbc5fceeef61ac67704243f64054e826999acbe
Xidian2850 <598109397@qq.com>
2019-08-23 14:53:05 +0800

    更新评论接口层

--- src/main/java/com/meiyou/service/impl/CommentServiceImpl.java
+++ src/main/java/com/meiyou/service/impl/CommentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.meiyou.service.impl;
 
 import com.meiyou.service.CommentService;
+import com.meiyou.utils.Msg;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,4 +13,27 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CommentServiceImpl implements CommentService {
+    /**
+     * 发布评论
+     *
+     * @param uid
+     * @param aid
+     * @param content
+     * @return
+     */
+    @Override
+    public Msg postComment(int uid, int aid, String content) {
+        return null;
+    }
+
+    /**
+     * 通过动态id获取所有评论
+     *
+     * @param aid
+     * @return
+     */
+    @Override
+    public Msg listCommentByAid(int aid) {
+        return null;
+    }
 }

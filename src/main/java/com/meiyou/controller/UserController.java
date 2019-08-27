c538daa73465dd12274e8427c870228f7903ec49
Administrator <2298598204@qq.com>
2019-08-27 17:17:14 +0800

    支付宝微信QQ登录

--- src/main/java/com/meiyou/controller/UserController.java
+++ src/main/java/com/meiyou/controller/UserController.java
@@ -195,4 +195,11 @@ public class UserController {
         return userService.registBindWeChat(uId,openId,accesssToken,phone,code,password,shareCode);
     }
 
+    @RequestMapping(value = "registBindQQ",method = RequestMethod.POST)
+    @ApiOperation("手机绑定QQ")
+    public Msg registBindQQ(int uId,String qqOpenId,String qqTokenn,String phone,String code,String password,String shareCode){
+        System.out.println("参数：qqOpenId:"+qqOpenId+",qqToken:"+qqTokenn+",uid:"+uId);
+        return userService.registBindWeChat(uId,qqOpenId,qqTokenn,phone,code,password,shareCode);
+    }
+
 }

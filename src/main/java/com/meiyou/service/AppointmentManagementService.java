d94bac1388355ace12a746f4e3d86401e72c2eaf
695591519 <695591519@qq.com>
2019-08-27 09:16:28 +0800

    增加约会后台管理接口

---
+++ src/main/java/com/meiyou/service/AppointmentManagementService.java
@@ -0,0 +1,10 @@
+package com.meiyou.service;
+
+/**
+ * @program: meiyou
+ * @description: 约会服务后台管理接口
+ * @author: JK
+ * @create: 2019-08-27 09:13
+ **/
+public interface AppointmentManagementService {
+}

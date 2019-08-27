9783edbb17a971d4671ca1e0bddb7a9340bdb49d
Honey0718 <zhongshuai9697@163.com>
2019-08-27 19:01:28 +0800

    修改完成shop附近查找不到

--- src/main/java/com/meiyou/controller/ShopController.java
+++ src/main/java/com/meiyou/controller/ShopController.java
@@ -40,24 +40,24 @@ public class ShopController {
                        @RequestParam("charge") Integer charge,
                        @RequestParam("time") Integer time,
                        @RequestParam("password") String password,
-                       @RequestParam("files") MultipartFile[] files,
+//                       @RequestParam("files") MultipartFile[] files,
                        Double longitude, Double latitude, HttpServletRequest request){
 
         //使用Hutool进行json操作
-        JSONArray array = JSONUtil.createArray();
-        for (MultipartFile file : files) {
-            Msg msg = FileUploadUtil.uploadUtil(file, "shop", request);
-            if (msg.getCode() == 100) {
-                array.add(msg.getExtend().get("path"));
-            }
-        }
-        if (array.size() == 0) {
-            return Msg.fail();
-        }
+//        JSONArray array = JSONUtil.createArray();
+//        for (MultipartFile file : files) {
+//            Msg msg = FileUploadUtil.uploadUtil(file, "shop", request);
+//            if (msg.getCode() == 100) {
+//                array.add(msg.getExtend().get("path"));
+//            }
+//        }
+//        if (array.size() == 0) {
+//            return Msg.fail();
+//        }
 
         Shop shop = new Shop();
         shop.setPublishId(publishId);
-        shop.setImgsUrl(array.toString());////以json数组的形式存图片
+//        shop.setImgsUrl(array.toString());////以json数组的形式存图片
         shop.setServiceArea(service_area);
         shop.setTravelTime(travel_time);
         shop.setCharge(charge);

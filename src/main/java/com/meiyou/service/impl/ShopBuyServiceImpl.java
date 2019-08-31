c7dc6c5768e6b355bf040e714856590849a91a9f
Administrator <2298598204@qq.com>
2019-08-31 15:19:07 +0800

    Merge branch 'master' of https://github.com/ffghfjh/meiyouapp
    # Conflicts:
    #	src/main/java/com/meiyou/service/impl/UserServiceImpl.java

--- src/main/java/com/meiyou/service/impl/ShopBuyServiceImpl.java
+++ src/main/java/com/meiyou/service/impl/ShopBuyServiceImpl.java
@@ -65,12 +65,12 @@ public class ShopBuyServiceImpl extends BaseServiceImpl implements ShopBuyServic
         Integer charges = charge * time;
 
         Msg msg = new Msg();
-        if(payWord.equals("")){
+        if(payWord == null){
             msg.setMsg("请设置支付密码!");
             msg.setCode(1000);
             return msg;
         }
-        if(!payWord.equals(password.toString())){
+        if(!password.toString().equals(payWord)){
             msg.setMsg("支付密码错误!");
             msg.setCode(1001);
             return msg;

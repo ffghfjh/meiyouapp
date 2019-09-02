2db1962598778fc988196c09fa13661b2b4437b3
Honey0718 <zhongshuai9697@163.com>
2019-09-02 11:03:32 +0800

    添加景点商家的置顶时间类型，添加取消发布退钱操作

--- src/test/java/com/meiyou/MeiyouApplicationTests.java
+++ src/test/java/com/meiyou/MeiyouApplicationTests.java
@@ -17,7 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
@@ -50,5 +53,35 @@ public class MeiyouApplicationTests extends BaseServiceImpl {
         example.createCriteria().andIdEqualTo(100);
         List<Club> result = clubMapper.selectByExample(example);
         System.out.println(result);
+    }
+
+
+    @Test
+    public void can(){
+//        Calendar calendar=Calendar.getInstance();
+//            calendar.setTime(new Date());
+//            System.out.println(calendar.get(Calendar.DAY_OF_MONTH));//今天的日期
+//            calendar.set(Calendar.DAY_OF_MONTH,calendar.get(Calendar.DAY_OF_MONTH)+1);//让日期加1
+//            System.out.println(calendar.get(Calendar.DATE));//加1之后的日期Top
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Club club = clubMapper.selectByPrimaryKey(56);
+        Date createTime = club.getCreateTime();
+        Date outTime = club.getOutTime();
+
+        long cha = outTime.getTime()-createTime.getTime();
+        long days = cha/(1000*60*60*24);
+        System.out.println(createTime);
+        System.out.println(outTime);
+        System.out.println(createTime.getTime());
+        System.out.println(outTime.getTime());
+        System.out.println("cha:"+cha);
+        System.out.println("days:"+days);
+
+        long hours = (cha-days*(1000 * 60 * 60 * 24))/(1000* 60 * 60);
+        long minutes = (cha-days*(1000 * 60 * 60 * 24)-hours*(1000* 60 * 60))/(1000* 60);
+        System.out.println(""+days+"天"+hours+"小时"+minutes+"分");
+
     }
 }

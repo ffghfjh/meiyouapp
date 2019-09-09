31051cbbd78ea217d0c5ae873e034dd937ee6e0e
Honey0718 <zhongshuai9697@163.com>
2019-09-09 15:03:43 +0800

    修改订单状态为已删除

--- src/test/java/com/meiyou/MeiyouApplicationTests.java
+++ src/test/java/com/meiyou/MeiyouApplicationTests.java
@@ -1,15 +1,15 @@
 package com.meiyou;
 
 import com.meiyou.config.QueueConfig;
+import com.meiyou.mapper.ClubBuyMapper;
 import com.meiyou.mapper.ClubMapper;
 import com.meiyou.mapper.RootMessageMapper;
 import com.meiyou.model.ExpirationMessagePostProcessor;
 import com.meiyou.model.ProcessReceiver;
-import com.meiyou.pojo.Club;
-import com.meiyou.pojo.ClubExample;
-import com.meiyou.pojo.RootMessage;
-import com.meiyou.pojo.RootMessageExample;
+import com.meiyou.myEnum.StateEnum;
+import com.meiyou.pojo.*;
 import com.meiyou.service.impl.BaseServiceImpl;
+import io.swagger.annotations.ApiOperation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -28,12 +28,20 @@ import java.util.concurrent.CountDownLatch;
 @SpringBootTest
 public class MeiyouApplicationTests extends BaseServiceImpl {
 
+    @Autowired
+    ClubBuyMapper clubBuyMapper;
 
     @Autowired
     RabbitTemplate rabbitTemplate;
     @Test
     public void contextLoads() {
-
+//        int clubBuyId = 1;
+//        ClubBuy clubBuy = new ClubBuy();
+//        clubBuy.setId(clubBuyId);
+//        clubBuy.setState(StateEnum.DELETE.getValue());
+//        clubBuy.setUpdateTime(new Date());
+//        clubBuyMapper.updateByPrimaryKeySelective(clubBuy);
     }
+
 
 }
